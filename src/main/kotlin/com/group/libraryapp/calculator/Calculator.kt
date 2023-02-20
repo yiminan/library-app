20e36667d3dd00614fb96835d08aab5b44fad41a
orgojy <giveawesome@gmail.com>
2023-02-20 23:46:44 +0900

    Update Calculator.kt to build operator methods

--- src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
+++ src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
@@ -1,4 +1,24 @@
 package com.group.libraryapp.calculator
 
-class Calculator {
+class Calculator constructor(
+    private var number: Int
+) {
+    fun add(operand: Int) {
+        this.number += operand
+    }
+
+    fun subtract(operand: Int) {
+        this.number -= operand
+    }
+
+    fun multiply(operand: Int) {
+        this.number *= operand
+    }
+
+    fun divide(operand: Int) {
+        if (operand == 0) {
+            throw IllegalArgumentException("Cannot divide by zero")
+        }
+        this.number /= operand
+    }
 }
