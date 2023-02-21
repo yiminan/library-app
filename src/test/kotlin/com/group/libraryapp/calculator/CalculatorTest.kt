04a4b699731807a11d0857a7b6f36504688ce80e
orgojy <giveawesome@gmail.com>
2023-02-21 23:16:26 +0900

    Update CalculatorTest.kt to add test case 'add'

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -1,7 +1,16 @@
 package com.group.libraryapp.calculator
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 class CalculatorTest {
 
+    @Test
+    fun addTest() {
+        val calculator = Calculator(5)
+
+        calculator.add(3)
+
+        assertEquals(Calculator(8), calculator)
+    }
 }
