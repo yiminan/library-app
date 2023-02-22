971bf69aed7308a993a392e93e6af6bb6d412c96
orgojy <giveawesome@gmail.com>
2023-02-22 23:42:03 +0900

    Update Calculator.kt to add test case 'subtract'

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -1,6 +1,6 @@
 package com.group.libraryapp.calculator
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class CalculatorTest {
@@ -12,5 +12,14 @@ class CalculatorTest {
         calculator.add(3)
 
         assertEquals(Calculator(8), calculator)
+    }
+
+    @Test
+    fun subtractTest() {
+        val calculator = Calculator(5)
+
+        calculator.subtract(3)
+
+        assertEquals(Calculator(2), calculator)
     }
 }
