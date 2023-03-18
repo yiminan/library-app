ddfe6e816b3b5e66977ca58ae1174e5beffdd2db
orgojy <giveawesome@gmail.com>
2023-03-18 23:50:51 +0900

    Minor JunitTest.kt to modify class comment

--- src/test/kotlin/com/group/libraryapp/JunitTest.kt
+++ src/test/kotlin/com/group/libraryapp/JunitTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
-@DisplayName("Junit Test")
+@DisplayName("Junit Tests")
 class JunitTest {
 
     private companion object {
