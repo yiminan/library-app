9210689a208acaf1e53dad3418d26b20db2d3a39
orgojy <giveawesome@gmail.com>
2023-03-23 15:25:04 +0900

    Update UserServiceTest to test saving user

--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -1,6 +1,9 @@
 package com.group.libraryapp.service.user
 
 import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -9,4 +12,18 @@ class UserServiceTest @Autowired constructor(
     private val userRepository: UserRepository,
     private val userService: UserService
 ) {
+
+    @Test
+    fun saveUserTest() {
+        // given
+        val request = UserCreateRequest("Ryan", null)
+        // when
+        userService.saveUser(request)
+
+        // then
+        val results = userRepository.findAll()
+        assertThat(results).hasSize(1)
+        assertThat(results[0].name).isEqualTo("Ryan")
+        assertThat(results[0].age).isNull()
+    }
 }
