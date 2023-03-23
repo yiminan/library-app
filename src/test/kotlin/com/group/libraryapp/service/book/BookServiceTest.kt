2d26abeffda6df8a547e961e45ccbdcfea5eaba8
orgojy <giveawesome@gmail.com>
2023-03-23 17:02:23 +0900

    Update BookServiceTest.kt to add test case 'saveBookTest'

--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -1,6 +1,9 @@
 package com.group.libraryapp.service.book
 
 import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.dto.book.request.BookRequest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -9,4 +12,16 @@ class BookServiceTest @Autowired constructor(
     private val bookService: BookService,
     private val bookRepository: BookRepository,
 ) {
+
+    @Test
+    fun saveBookTest() {
+        // given
+        val request = BookRequest("Communism")
+        // when
+        bookService.saveBook(request)
+        // then
+        val books = bookRepository.findAll()
+        assertThat(books).hasSize(1)
+        assertThat(books[0].name).isEqualTo("Communism")
+    }
 }
