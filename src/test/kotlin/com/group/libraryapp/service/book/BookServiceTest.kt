a0b1f523fd77963e50b5781cb8ed01d231db87e9
orgojy <giveawesome@gmail.com>
2023-03-23 16:58:27 +0900

    Update BookServiceTest.kt to inject components for domain 'Book'

--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -1,4 +1,12 @@
 package com.group.libraryapp.service.book
 
-class BookServiceTest {
+import com.group.libraryapp.domain.book.BookRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookServiceTest @Autowired constructor(
+    private val bookService: BookService,
+    private val bookRepository: BookRepository,
+) {
 }
