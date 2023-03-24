4bc816f32a0fec583e34a7c955b691a0d8096228
orgojy <giveawesome@gmail.com>
2023-03-24 17:41:17 +0900

    Update BookLoanRequest.java to add primary constructor

--- src/main/java/com/group/libraryapp/dto/book/request/BookLoanRequest.java
+++ src/main/java/com/group/libraryapp/dto/book/request/BookLoanRequest.java
@@ -1,16 +1,23 @@
 package com.group.libraryapp.dto.book.request;
 
+import org.jetbrains.annotations.NotNull;
+
 public class BookLoanRequest {
 
-  private String userName;
-  private String bookName;
+    private String userName;
+    private String bookName;
 
-  public String getUserName() {
-    return userName;
-  }
+    public BookLoanRequest(@NotNull String userName, @NotNull String bookName) {
+        this.userName = userName;
+        this.bookName = bookName;
+    }
 
-  public String getBookName() {
-    return bookName;
-  }
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
 
 }
