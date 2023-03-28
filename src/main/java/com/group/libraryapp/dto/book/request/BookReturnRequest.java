69cfbddb6213bc437eb3bbed510332a3680ac960
orgojy <giveawesome@gmail.com>
2023-03-28 19:28:25 +0900

    Update BookReturnRequest to add primary constructor

--- src/main/java/com/group/libraryapp/dto/book/request/BookReturnRequest.java
+++ src/main/java/com/group/libraryapp/dto/book/request/BookReturnRequest.java
@@ -1,9 +1,16 @@
 package com.group.libraryapp.dto.book.request;
 
+import org.jetbrains.annotations.NotNull;
+
 public class BookReturnRequest {
 
     private String userName;
     private String bookName;
+
+    public BookReturnRequest(@NotNull String userName, @NotNull String bookName) {
+        this.userName = userName;
+        this.bookName = bookName;
+    }
 
     public String getUserName() {
         return userName;
