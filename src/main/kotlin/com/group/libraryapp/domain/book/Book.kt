cc163b6e41b99ef87c88d78c3b4082a3571f79c7
orgojy <giveawesome@gmail.com>
2023-03-30 15:46:16 +0900

    Add Book.kt to use entity for 'book'

---
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -0,0 +1,23 @@
+package com.group.libraryapp.domain.book
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Book(
+    val name: String,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,// default 값이 있는 경우 아래에 배열하는 것이 기본 컨벤션
+) {
+
+    init {
+        if(name.isBlank()) {
+            throw IllegalArgumentException("Book name must not be blank")
+        }
+    }
+
+}
